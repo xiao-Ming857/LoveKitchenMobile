c5ee8adbd88a7bc92355621b9eb48233716cfbde
xiao-Ming857 <69382210+xiao-Ming857@users.noreply.github.com>
2022-12-14 21:39:33 +0800

    Add files via upload

---
+++ js/start_onlyyou.js
@@ -0,0 +1,5 @@
+function init_onlyyou(){
+    // 初始化两个div的高度
+    $("#div_onlyyou").css({"height":$(window).height()+260+"px"});
+    $("#div_oy_inner").css({"height":$(window).height()+260+"px"});
+}
